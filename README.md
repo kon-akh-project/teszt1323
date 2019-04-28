b55531bfbba38db6676bcc8e07e5675f05c6fb54
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-28 23:01:24 +0000

    Deploy kon-akh-project/teszt1323 to github.com/kon-akh-project/teszt1323.git:gh-pages

---
+++ README.md
@@ -0,0 +1,22 @@
+# Kon-Akh
+Kon Akh is a groundbraking platformer game set to be released at the end of this semester.
+
+Tools:
+Travis CI test:
+[![Build Status](https://travis-ci.com/kon-akh-project/teszt1323.svg?branch=master)](https://travis-ci.com/kon-akh-project/teszt1323)
+
+FindBugs
+
+Javadoc
+
+
+Build:
+  - cd src
+  - javac Main/Game.java
+  - java Main.Game
+ 
+Test on Ant:
+  install Ant on your Desktop
+  
+cmd:
+  ant
---
+++ test/Entity/PlayerTest.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import TileMap.*;
+import Entity.Player;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Krisztian
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+
+    
+    @Test
+    public void testGetHealth() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+        
+        int actual = player.getHealth();
+        int expected = 5;
+        assertEquals(expected,actual);
+    }
+
+    
+    @Test
+    public void testGetMaxHealth() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+        
+        int actual = player.getMaxHealth();
+        int expected = 5;
+        assertEquals(expected,actual);        
+    }
+
+   
+    @Test
+    public void testGetFire() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+        
+        int actual = player.getFire();
+        int expected = 2500;
+        assertEquals(expected,actual);         
+    }
+
+  
+    @Test
+    public void testGetMaxFire() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+        
+        int actual = player.getMaxFire();
+        int expected = 2500;
+        assertEquals(expected,actual);          
+    }
+
+    
+    @Test
+    public void testSetFiring() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+        
+        player.setFiring();
+        int expected = 2500;
+        assertTrue(player.getFiring());         
+    }
+
+   
+    @Test
+    public void testSetScratching() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+
+        player.setScratching();
+        assertTrue(player.getScratching());  
+    }
+
+    
+    @Test
+    public void testSetGliding() {
+        TileMap tileMap = new TileMap(50);
+        tileMap.setPosition(0, 0);
+
+        Player player = new Player(tileMap);
+
+        player.setGliding(true);
+        assertTrue(player.getGliding());          
+        
+    }
+
+
+    
+}
